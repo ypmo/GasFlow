d11e991e462b9848630f651e004df767626e0b04
Sergey Ilyin <silyinnur@gmail.com>
2021-01-28 16:37:52 +0500

    step for generation

--- src/GasFlow.Sim.PipeSim/NetworkGenerator.cs
+++ src/GasFlow.Sim.PipeSim/NetworkGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using Energistics.DataAccess.PRODML210;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,7 +17,22 @@ namespace GasFlow.Sim.PipeSim
 
         public string Generate(ProductFlowModel model, ProductVolume volume)
         {
-
+         
+           var network = model.Network.FirstOrDefault() ?? throw new ArgumentNullException(nameof(model));
+        foreach(var unit in     network.Unit)
+            {
+          
+                if ( unit.Facility.Kind==Energistics.DataAccess.PRODML210.ReferenceData.ReportingFacility.pipeline )
+                {
+  var facility=    volume.Facility.FirstOrDefault(t => t.Unit == unit.Uid);
+                   var profile= facility.ParameterSet.First(t => t.Name ==Energistics.DataAccess.PRODML210.ReferenceData.FacilityParameter.lengthclass );
+                    profile.MeasureClass = Energistics.DataAccess.PRODML210.ReferenceData.MeasureClass.length;
+                    foreach (var curve in profile.CurveDefinition )
+                    {
+                        
+                    }
+                }
+            }
             throw new NotImplementedException();
         }
     }
---
+++ test/GasFlow.Sim.PipeSim.Test/NetworkGeneratorTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GasFlow.Sim.PipeSim.Test
+{
+    public class NetworkGeneratorTests
+    {
+    }
+}
